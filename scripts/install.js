a3caacdb37c73066444893b1267d4ee0ea4a4860
Bermi Ferrer <bermi@bermilabs.com>
2014-12-12 01:08:31 +0100

    Linting code issues reported by (make lint)

--- scripts/install.js
+++ scripts/install.js
@@ -3,13 +3,14 @@ if (!process.env.SAUCE_CONNECT_DOWNLOAD_ON_INSTALL) {
   return;
 }
 
-var sauceConnectLauncher = require('..');
+var sauceConnectLauncher = require("../");
 
 sauceConnectLauncher.download({
   logger: console.log.bind(console),
-}, function(error) {
-  if (error) {
-    console.log('Failed to download sauce connect binary:', error);
-    console.log('sauce-connect-launcher will attempt to re-download next time it is run.');
+}, function (err) {
+  if (err) {
+    console.log("Failed to download sauce connect binary:", err);
+    console.log("sauce-connect-launcher will attempt to re-download " +
+      "next time it is run.");
   }
 });
